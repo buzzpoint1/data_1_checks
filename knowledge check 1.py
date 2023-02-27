8365ed05d9039dc59fd680c778785907bc25df9a
buzzpoint <sajid_waris@yahoo.com>
2023-02-26 19:02:25 -0500

    assignment

--- knowledge check 1.py
+++ knowledge check 1.py
@@ -1,8 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 
 print('Hello World')
+
+
+# In[4]:
+
+
+fruits = ["apple","oranges", 35,
+         {"Apple":5,
+          "Oranges": 8,
+          "Weight":35
+         }
+         ]
+
+
+# In[27]:
+
+
+print(fruits[3]['Weight'])
+
+
+# In[28]:
+
+
+for items in fruits:
+    print(items)
+
+
+# In[32]:
+
+
+cars_tuple = ('ford','toyota','kia','honda')
+
+
+# In[33]:
+
+
+print(cars_tuple[1])
+
+
+# In[34]:
+
+
+cars_dic = {'ford':'edge','toyota':['camry','civic']}
+
+
+# In[35]:
+
+
+print(cars_dic['toyota'])
+
+
+# In[ ]:
+
+
+
 
